f7490da358fe4ec7d7bf0f32d819e71838f49aa4
YousefAlsuliman <443105666@student.ksu.edu.sa>
2025-09-02 16:38:15 +0300

    fasdf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ContactComponent } from '../contact/contact.component';
-
+//
 @Component({
   selector: 'app-home',
   imports: [AboutComponent, ProjectsComponent, ContactComponent],
