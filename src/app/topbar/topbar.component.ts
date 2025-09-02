4657f75cf6d47cbe1b4324792a31c602096cae98
YousefAlsuliman <443105666@student.ksu.edu.sa>
2025-09-02 21:19:05 +0300

    dropdown

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-topbar',
   imports: [],
   templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.css'
+  styleUrl: './topbar.component.css',
 })
 export class TopbarComponent {
+  dropDownActive: boolean = false;
 
+  onDropDown() {
+    if (this.dropDownActive === false) {
+      this.dropDownActive = true;
+    } else {
+      this.dropDownActive = false;
+    }
+  }
 }
